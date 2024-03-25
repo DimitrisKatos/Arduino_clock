8acffa2bb46d3b0edec0b00f0f301ffe6d3f532e
DimitrisKatos <dimitriskatos6@gmail.com>
2024-03-25 21:48:24 +0200

    arduino clock

---
+++ README.md
@@ -0,0 +1,26 @@
+# Arduino clock without RTC.
+
+In this repository we will create a arduino clock without RTC sensor. We will use a lcd to display the time, the date, a counter and also to set an alarm.Also we will use 6 pins to manually change the time, data and alarm. 
+
+---
+In the following image you can see how we connect the lcd and the pins to arduino. The scematics has been designed using tinkercad.
+
+![Poll Mockup](./arduino.png)
+
+---
+## Push Buttons.
+
+You can use the pins to make changes to the system using the following rules:
+- Push Button 1 changes the minutes.
+- Push Button 2 changes the hours. 
+- Push button 3 changes the days.
+- Push button 4 changes the months. To change the years you must do it by pressing this button 12 times.
+- Push button 5 and hold it, then  the second row of the lcd display changes to alarm. There by pressing the first button you increase the minutes and with the button 2 change hours for the alarm.
+- Push button 6 and hold it, then  the second row of the lcd display changes to counter.
+
+---
+## Suggestions.
+- You can improve the code by adding specific days to every month.
+- The second row of the lcd, change only when we hold the Button 5(Alarm)
+ and the Button 6 (Counter).
+- You can increase the hours and minutes only by one in every second. 
--- newproject.ino
+++ newproject.ino
@@ -1,26 +1,36 @@
-//Είσοδος βιβλιθοθηκών που χρησιμοποιήθηκαν
+// Create a arduino clock without rtc.
+// Import the libraries
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-//Ορισμός μεταβλητών για την ώρα και ημερομηνία
+
+// Define the variables for the time and date.
 int sec =0 , min =0, hour=0 ,day=1 ,month=1 ,year=2022;
-//Ορισμός μεταβλητών που χρησιμεύουν μόνο στο ξυπνητήρι
+
+// Define the variables for the alarm.
 int Hour=1, Min=0;
-//Ορισμός μεταβλητών που χρησιμοποιούνται μόνο για το χρονόμετρο
+
+// Define variables that is used only for the counter. 
 int MIN =0 ,SEC=0;
-//Καθορισμός των pin που χρησιμοποιούνται ως buttons
+
+// Define the pins for the buttons. 
 int pushbutton1 = 7 , pushbutton2= 8, pushbutton3 =9,pushbutton4 = 10, pushbutton5=4 , pushbutton6=5;
-//Καθορισμός του Buzzer
+
+// Define the pin for the buzzer.
 int buzzer=2 ;
+
+// Define the varible now as millis. 
 unsigned long now=millis();
 int event=1000;
-//Η συνάρτηση setup θα τρέξει μόνο μια φορά στην αρχή του προγράμματος.
+
 void setup() {
- //Αρχικοποίηση της lcd οθόνης
+ // initialization of the lcd display.
  lcd.begin();
- //Έναρξη σειριακής επικοινωνίας
+
+ // Enable the serial communication.
  Serial.begin(9600);
-//Oρισμός των Buttons και του Buzzer σαν είσοδος ή έξοδος
+
+// Define the buttons and buzzer as output or inputs.
   pinMode(pushbutton1,INPUT_PULLUP);
   pinMode(pushbutton2,INPUT_PULLUP);
   pinMode(pushbutton3,INPUT_PULLUP);
@@ -30,9 +40,7 @@ void setup() {
   pinMode(buzzer,OUTPUT);
 }
 
-//Η συνάρτηση loop θα επαναλαμβάνεται συνέχεια σαν ατέρμονος βρόγχος
 void loop() {
-  //Ανάγνωση των ψηφιακών θυρών
   int buttonState1=digitalRead(pushbutton1);
   int buttonState2=digitalRead(pushbutton2);
   int buttonState3=digitalRead(pushbutton3);
@@ -40,14 +48,13 @@ void loop() {
   int buttonState5=digitalRead(pushbutton5);
   int buttonState6=digitalRead(pushbutton6);
 
-  
-  //Στη συνάρτηση long αναθέτω το χρόνο απο τη στιγμή που ξεκίνησε το πρόγραμμα
- 
+   
   if(millis()-now>=event){
     Serial.println(millis()-now);
     now=millis();
     sec=sec+1;
-    //Οπτικοποίηση της πρώτης γραμμής της lcd οθόνης
+
+    // First raw of the lcd display for the hour.
     lcd.setCursor(0,0);
     lcd.print("TIME:");
     lcd.print(hour);
@@ -56,7 +63,9 @@ void loop() {
     lcd.print(":");
     lcd.print(sec);
     lcd.print("        ");
-    //Τοποθέτηση ορίων και μηδενισμός στις μεταβλητές του χρόνου και της ημερομηνίας
+
+    // We define the boundaries for the seconds, minutes, hours and date.
+
     if(sec==59){
       min=min+1;
       sec=-1;
@@ -77,34 +86,36 @@ void loop() {
       year=year+1;
       month=1;
     }
-  //Ξεκίνημα αλγορίθμου για τη χρησιμότητητα των κουμπιών 
-  //Αρχή με το κουμπι1 που αντιστοιχει στην αλλαγή των λεπτων
+
+  // The first push button help us to change the minutes of the clock.
   if(buttonState1==HIGH && buttonState6==LOW && buttonState5==LOW){
     min=min+1;
     }
  
-  //Το κουμπί 2 αντιστοιχεί στην αλλαγή των ωρών
+  // The second push button help for changing the hours.
   if(buttonState2==HIGH && buttonState6==LOW && buttonState5==LOW){
     hour=hour+1;
     } 
-  //Το κουμπι 3 αντιστοιχεί στην αλλαγή των ημερών
+
+  // The third push button changes the days.
   if(buttonState3==HIGH){
     day=day+1;
     }
-  //Τέλος το κουμπί 3 αντιστοιχεί στην αύξηση των μηνών
+    
+  // The fourth button changes the months.
   if(buttonState4==HIGH&& buttonState5==LOW){
     month=month+1;
-    //Αμα θελήσω να αλλάξω το έτος πρέπει να το κάνω μέσω του pushbutton4 
-    //Δηλαδή να αυξήσω του μήνες
+
+    // If you want to change the year, you are doing it by increasing the months over 12.
     if(month==12){
       year=year+1;
     }
   }
-  //Οπτικοποίηση της δεύτερης γραμμής της lcd οθόνης
-  //Δηλαδή αν δεν πατάω το Button 5 και 6 τότε στη δεύτερη γραμμή εμφανίζεται η ημερομηνία 
-  //Αν πατάω το Button5 και δεν πατάω το Button 6 τότε εμφανίζεται το ξυπνητήρι και μπορώ να το ρυθμίζω στην ώρα που εγώ θέλω
-  //Αν πατάω το Button6 και δεν πατάω το Button 5 τότε εμφανίζεται το χρονόμετρο με ακρίβεια δευτερολέπτου  
-    if(buttonState5==LOW &&buttonState6==LOW) { //ΗΜΕΡΟΜΗΝΊΑ
+  
+  // The second raw of the lcd display is for Date, alram and counter.
+  // At first the second row will display the Date.
+
+  if(buttonState5==LOW &&buttonState6==LOW) { //Date
     lcd.setCursor(0, 1);
     lcd.print("DATE:");
     lcd.print(day);
@@ -113,11 +124,14 @@ void loop() {
     lcd.print(":");
     lcd.print(year);
     lcd.print("         ");
-    //Όποτε βγαίνω από το χρονόμετρο ,οι μεταβλητές που χρησιμοποιώ εκεί θα μηδενίζονται.
+
+    // When i return to the Date format, the counter return to zero.
     SEC=0;
     MIN=0;
     }
-     if(buttonState5==HIGH && buttonState6==LOW){ //ΞΥΠΝΗΤΗΡΙ
+
+  // If you press button 5, in the second row you can format the alarm.
+  if(buttonState5==HIGH && buttonState6==LOW){ //Alarm.
 
       lcd.setCursor(0,1);
       lcd.print("ALARM:");
@@ -134,13 +148,17 @@ void loop() {
         if(Hour==24) Hour=0;
       }
     }
-     if (buttonState6==HIGH && buttonState5==LOW){ // ΧΡΟΝΌΜΕΤΡΟ
-        if(buttonState2==HIGH){
-          hour=0;
-          min=0;
-          day=1;
-          month=1;
-          year=2022;
+
+  // Enable counter by pressing the button 6.
+  if (buttonState6==HIGH && buttonState5==LOW){ // Counter.
+
+      // In this mode, you can press the second button to refresh the hour and date.
+      if(buttonState2==HIGH){
+        hour=0;
+        min=0;
+        day=1;
+        month=1;
+        year=2022;
         }
         lcd.setCursor(0,1);
         lcd.print("TIMER:");
@@ -156,6 +174,7 @@ void loop() {
         }
       }
      }
+  // The buzzer will be set to HIGH.
   if(min==Min && hour==Hour){
     tone(buzzer,5000);}
     else noTone(buzzer);
